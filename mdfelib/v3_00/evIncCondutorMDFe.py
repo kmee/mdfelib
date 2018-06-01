60b4fc65b3f831ab7599119f769cf4d4890b06bc
Luis Felipe Mileo <mileo@kmee.com.br>
2018-06-01 17:26:41 -0300

    classes and doc generated with ./generate

--- mdfelib/v3_00/evIncCondutorMDFe.py
+++ mdfelib/v3_00/evIncCondutorMDFe.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Thu May 31 13:52:48 2018 by generateDS.py version 2.29.2.
+# Generated Fri Jun  1 17:25:24 2018 by generateDS.py version 2.29.2.
 # Python 3.6.5 (default, Apr  1 2018, 05:46:30)  [GCC 7.3.0]
 #
 # Command line options:
